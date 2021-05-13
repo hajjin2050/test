40e6c81a5bc4e043e857e0ef7db1faaffeb134a6
moonsh2050 <76682924+moonsh2050@users.noreply.github.com>
2021-05-13 20:25:49 +0900

    Add files via upload

---
+++ dacon/solar/py/dnn_.py
@@ -0,0 +1,249 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import lightgbm as lgb
+import autokeras as ak
+energy = pd.read_csv('./data/energy.csv')
+dangjin_fcst = pd.read_csv('./data/dangjin_fcst_data.csv')
+ulsan_fcst = pd.read_csv('./data/ulsan_fcst_data.csv')
+
+print(energy.head())
+print(dangjin_fcst.head())
+print(ulsan_fcst.head())
+
+dangjin_fcst['Forecast_time'] = pd.to_datetime(dangjin_fcst['Forecast time'])
+ulsan_fcst['Forecast_time'] = pd.to_datetime(ulsan_fcst['Forecast time'])
+
+dangjin_fcst_14 = dangjin_fcst[dangjin_fcst['Forecast_time'].dt.hour==14]
+ulsan_fcst_14 = ulsan_fcst[ulsan_fcst['Forecast_time'].dt.hour==14]
+
+dangjin_fcst_14 = dangjin_fcst_14[(dangjin_fcst_14['forecast']>=10)&(dangjin_fcst_14['forecast']<=33)]
+ulsan_fcst_14 = ulsan_fcst_14[(ulsan_fcst_14['forecast']>=10)&(ulsan_fcst_14['forecast']<=33)]
+
+def to_date(x):
+    return pd.DateOffset(hours=x)
+
+dangjin_fcst_14['Forecast_time'] = dangjin_fcst_14['Forecast_time'] + dangjin_fcst_14['forecast'].map(to_date)
+ulsan_fcst_14['Forecast_time'] = ulsan_fcst_14['Forecast_time'] + ulsan_fcst_14['forecast'].map(to_date)
+
+dangjin_fcst_14 = dangjin_fcst_14[['Forecast_time', 'Temperature', 'Humidity', 'WindSpeed', 'WindDirection', 'Cloud']]
+ulsan_fcst_14 = ulsan_fcst_14[['Forecast_time', 'Temperature', 'Humidity', 'WindSpeed', 'WindDirection', 'Cloud']]
+
+dangjin_fcst_14_ = pd.DataFrame()
+dangjin_fcst_14_['Forecast_time'] = pd.date_range(start='2018-03-02 00:00:00', end='2021-03-01 23:00:00', freq='H')
+
+ulsan_fcst_14_ = pd.DataFrame()
+ulsan_fcst_14_['Forecast_time'] = pd.date_range(start='2018-03-02 00:00:00', end='2021-03-01 23:00:00', freq='H')
+
+dangjin_fcst_14_ = pd.merge(dangjin_fcst_14_, dangjin_fcst_14, on='Forecast_time', how='outer')
+ulsan_fcst_14_ = pd.merge(ulsan_fcst_14_, ulsan_fcst_14, on='Forecast_time', how='outer')
+
+dangjin_fcst = dangjin_fcst_14_.interpolate()
+ulsan_fcst = ulsan_fcst_14_.interpolate()
+
+dangjin_fcst['Forecast_time'] = dangjin_fcst['Forecast_time'].astype(str)
+ulsan_fcst['Forecast_time'] = ulsan_fcst['Forecast_time'].astype(str)
+
+print(dangjin_fcst.head())
+print(ulsan_fcst.head())
+
+def train_datast(energy_df, fcst_df, target):
+    # 일기 예보 있는 날짜만 선택
+    energy = energy_df.loc[24:]
+    energy.index = range(energy.shape[0])
+    
+    # 발전량 데이터가 있는 날짜만 선택
+    fcst = fcst_df.loc[:25608-1]
+    fcst.index = range(fcst.shape[0])
+    
+    # 발전량과 일기예보 연결
+    concat_df = pd.concat([energy, fcst], axis=1)
+    
+    # 예보 시간 및 날짜 정보 feature로 추가
+    concat_df['date'] = concat_df['Forecast_time'].str.split(' ').str[0]
+    concat_df['hour'] = concat_df['Forecast_time'].str.split(' ').str[1].str.split(':').str[0].astype(int)
+    
+    concat_df['year'] = concat_df['date'].str.split('-').str[0].astype(int)
+    concat_df['month'] = concat_df['date'].str.split('-').str[1].astype(int)
+    concat_df['day'] = concat_df['date'].str.split('-').str[2].astype(int)
+    
+    # 예보 시간, 날짜, 기상 예보 및 발전량 선택
+    feature_df = concat_df[['year', 'month', 'day', 'hour', 'Temperature', 'Humidity', 'WindSpeed', 'WindDirection', 'Cloud', target]]
+    
+    # 마지막 30일을 검증데이터셋으로 나머지를 학습 데이터셋으로 선택
+    train_df = feature_df.iloc[:-24*30]
+    val_df = feature_df.iloc[-24*30:]
+    
+    # 발전량이 0인 데이터를 제외
+    train_df = train_df[train_df[target]!=0]
+    
+    train_x = train_df.loc[:, 'year':'Cloud'].to_numpy()
+    train_y = train_df[target].to_numpy()
+    
+    val_x = val_df.loc[:, 'year':'Cloud'].to_numpy()
+    val_y = val_df[target].to_numpy()
+    
+    return train_x, train_y, val_x, val_y
+
+def nmae_10(y_pred, dataset):
+    y_true = dataset.get_label()
+    
+    absolute_error = abs(y_true - y_pred)
+    absolute_error /= capacity
+    
+    target_idx = np.where(y_true>=capacity*0.1)
+    
+    nmae = 100 * absolute_error[target_idx].mean()
+    
+    return 'score', nmae, False
+
+#validation
+def sola_nmae(answer, pred):
+    absolute_error = np.abs(answer - pred)
+    
+    absolute_error /= capacity
+    
+    target_idx = np.where(answer>=capacity*0.1)
+    
+    nmae = 100 * absolute_error[target_idx].mean()
+    
+    return nmae
+print(dangjin_fcst)
+from keras.layers import Dense, Dropout,LSTM
+from keras.models import Sequential
+
+train_x, train_y, val_x, val_y = train_datast(energy, dangjin_fcst, target='dangjin_floating')
+
+train_x = train_x.reshape(train_x[0], 18,1)
+model = Sequential()
+model.add(LSTM(1280,input_shape=(9,), activation='relu'))``
+model.add(Dropout(0.3))
+model.add(Dense(640, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(320, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(160, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(40, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(1, activation='sigmoid'))
+
+#당진수상태양광 에측 모델 학습
+
+train_x, train_y, val_x, val_y = train_datast(energy, dangjin_fcst, target='dangjin_floating')
+model.compile(loss = 'mae', optimizer = 'rmsprop', metrics = ['mse'])
+dangjin_floating_model = model.fit(train_x, train_y, validation_split = 0.2, epochs = 80, batch_size = 32)
+dangjin_floating_model = model.predict(val_x)
+'''
+plt.figure(figsize=(20,5))
+plt.plot(val_y, label='true')
+plt.plot(pred, label='pred')
+plt.legend()
+plt.show()
+print('CV Score : ', sola_nmae(val_y, pred))
+
+
+
+#당진자재창고태양광 예측 모델 학습
+train_x, train_y, val_x, val_y = train_datast(energy, dangjin_fcst, target='dangjin_warehouse')
+print(train_x.shape)
+print(train_y.shape)
+train_dataset = lgb.Dataset(train_x, train_y)
+val_dataset = lgb.Dataset(val_x, val_y)
+capacity = 700
+dangjin_warehouse_model = model.predict(train_x, train_dataset, 10000, val_dataset, feval=nmae_10, verbose_eval=500, early_stopping_rounds=100)
+
+pred = dangjin_warehouse_model.predict(val_x)
+
+plt.figure(figsize=(20,5))
+plt.plot(val_y, label='true')
+plt.plot(pred, label='pred')
+plt.legend()
+plt.show()
+print('CV Score : ', sola_nmae(val_y, pred))
+
+#당진태양광 예측 모델 학습
+train_x, train_y, val_x, val_y = train_datast(energy, dangjin_fcst, target='dangjin')
+train_dataset = lgb.Dataset(train_x, train_y)
+val_dataset = lgb.Dataset(val_x, val_y)
+capacity = 1000
+dangjin_model = lgb.train(params, train_dataset, 10000, val_dataset, feval=nmae_10, verbose_eval=500, early_stopping_rounds=100)
+
+
+pred = dangjin_model.predict(val_x)
+
+plt.figure(figsize=(20,5))
+plt.plot(val_y, label='true')
+plt.plot(pred, label='pred')
+plt.legend()
+plt.show()
+print('CV Score : ', sola_nmae(val_y, pred))
+
+#울산태양광 예측 모델 학습
+train_x, train_y, val_x, val_y = train_datast(energy, ulsan_fcst, target='ulsan')
+train_dataset = lgb.Dataset(train_x, train_y)
+val_dataset = lgb.Dataset(val_x, val_y)
+capacity = 500
+ulsan_model = lgb.train(params, train_dataset, 10000, val_dataset, feval=nmae_10, verbose_eval=500, early_stopping_rounds=100)
+
+pred = ulsan_model.predict(val_x)
+
+plt.figure(figsize=(20,5))
+plt.plot(val_y, label='true')
+plt.plot(pred, label='pred')
+plt.legend()
+plt.show()
+print('CV Score : ', sola_nmae(val_y, pred))
+
+#테스트 데이터 전처리
+def test_datast(fcst_df):
+    start = '2021-02-01 00:00:00'
+    end = '2021-02-28 23:00:00'
+    
+    start_idx = fcst_df[fcst_df['Forecast_time']==start].index[0]
+    end_idx = fcst_df[fcst_df['Forecast_time']==end].index[0]
+    
+    test_df = fcst_df.loc[start_idx:end_idx, :].copy()
+    
+    test_df['date'] = test_df['Forecast_time'].str.split(' ').str[0]
+    test_df['hour'] = test_df['Forecast_time'].str.split(' ').str[1].str.split(':').str[0].astype(int)
+    
+    test_df['year'] = test_df['date'].str.split('-').str[0].astype(int)
+    test_df['month'] = test_df['date'].str.split('-').str[1].astype(int)
+    test_df['day'] = test_df['date'].str.split('-').str[2].astype(int)
+    
+    test_df = test_df[['year', 'month', 'day', 'hour', 'Temperature', 'Humidity', 'WindSpeed', 'WindDirection', 'Cloud']]
+    
+    test_x = test_df.to_numpy()
+    
+    return test_x
+
+dangjin_test = test_datast(dangjin_fcst)
+ulsan_test = test_datast(ulsan_fcst)
+
+#각 발전소 발전량 추론
+dangjin_floating_pred = dangjin_floating_model.predict(dangjin_test)
+dangjin_warehouse_pred = dangjin_warehouse_model.predict(dangjin_test)
+dangjin_pred = dangjin_model.predict(dangjin_test)
+ulsan_pred = ulsan_model.predict(ulsan_test)
+
+plt.figure(figsize=(20,5))
+plt.plot(dangjin_floating_pred, label = 'dangjin_floating_pred')
+plt.plot(dangjin_warehouse_pred, label = 'dangjin_warehouse_pred')
+plt.plot(dangjin_pred, label = 'dangjin_pred')
+plt.plot(ulsan_pred, label = 'ulsan_pred')
+plt.legend()
+plt.show()
+
+#submit
+submission = pd.read_csv('./data/sample_submission.csv')
+submission.iloc[:24*28, 1] = dangjin_floating_pred
+submission.iloc[:24*28, 2] = dangjin_warehouse_pred
+submission.iloc[:24*28, 3] = dangjin_pred
+submission.iloc[:24*28, 4] = ulsan_pred
+
+print(submission)
+
+submission.to_csv('./submit/dacon_baseline.csv', index=False)
+
+'''
